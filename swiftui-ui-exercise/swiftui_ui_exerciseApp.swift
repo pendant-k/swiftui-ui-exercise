894617f7219dcf51fac6a74f6fbf2a28c239b207
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-01-11 18:02:06 +0900

    Initial Commit

---
+++ swiftui-ui-exercise/swiftui_ui_exerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_ui_exerciseApp.swift
+//  swiftui-ui-exercise
+//
+//  Created by Donghan Kim on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_ui_exerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
